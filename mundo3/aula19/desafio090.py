cbfa7a891d2713560b0377bd70ddf6f551bb0c55
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-03-15 18:34:51 -0300

    Exercicios de python (aula 19)

---
+++ mundo3/aula19/desafio090.py
@@ -0,0 +1,18 @@
+print(f'{" DESAFIO 090 ":=^29}')
+
+student_status = dict()
+
+student_name = input(f'Nome do aluno: ')
+student_avarage_grade = float(input(f'Média do aluno: '))
+
+student_status['Nome'] = student_name
+student_status['Média'] = student_avarage_grade
+
+if student_avarage_grade >= 7:
+    student_status['Situação'] = 'Aprovado'
+else:
+    student_status['Situação'] = 'Reprovado'
+
+for k, v in student_status.items():
+    print(f'{k}: {v}')
+
