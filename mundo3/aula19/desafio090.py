357202b53330e84393608c9c77c51d1588c8ded0
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-03-16 12:20:44 -0300

    Exercícios de Python (Aula 19)

--- mundo3/aula19/desafio090.py
+++ mundo3/aula19/desafio090.py
@@ -2,17 +2,16 @@ print(f'{" DESAFIO 090 ":=^29}')
 
 student_status = dict()
 
-student_name = input(f'Nome do aluno: ')
-student_avarage_grade = float(input(f'Média do aluno: '))
+student_name = input(f"Nome do aluno: ")
+student_avarage_grade = float(input(f"Média do aluno: "))
 
-student_status['Nome'] = student_name
-student_status['Média'] = student_avarage_grade
+student_status["Nome"] = student_name
+student_status["Média"] = student_avarage_grade
 
 if student_avarage_grade >= 7:
-    student_status['Situação'] = 'Aprovado'
+    student_status["Situação"] = "Aprovado"
 else:
-    student_status['Situação'] = 'Reprovado'
+    student_status["Situação"] = "Reprovado"
 
 for k, v in student_status.items():
-    print(f'{k}: {v}')
-
+    print(f"{k}: {v}")
---
+++ mundo3/aula19/desafio091.py
@@ -0,0 +1,55 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+print(f'{" DESAFIO 091 ":=^29}')
+
+dice_players: dict[str, int] = {
+    "Player 1": randint(1, 6),
+    "Player 2": randint(1, 6),
+    "Player 3": randint(1, 6),
+    "Player 4": randint(1, 6),
+}
+
+print("Sorteando...", end="\r")
+sleep(2)
+print("Valores sorteados:")
+
+for player, dice in dice_players.items():
+    print(f"{player}: {dice}")
+    sleep(1)
+
+### SORTED() METHOD WITH ITEMGETTER ###
+ranking: list[tuple[str, int]] = sorted(
+    dice_players.items(), key=itemgetter(1), reverse=True
+)
+
+print(f'\n{" RANKING DE JOGADORES ":=^26}')
+for i, player in enumerate(ranking):
+    print(f"{i + 1}ª {player[0]}: {player[1]}")
+    sleep(1)
+
+
+### USING THE SORTED() METHOD ###
+# for player in sorted(dice_players, key=dice_players.get, reverse=True):
+#     print(player, dice_players[player])
+
+
+### MANUAL SORTING ###
+# dice_items: list[tuple[str, int]] = list(dice_players.items())
+
+# for i in range(len(dice_items)):
+#     min_index: int = i
+#     for j in range(i + 1, len(dice_items)):
+#         if dice_items[j][1] < dice_items[min_index][1]:
+#             min_index = j
+#     dice_items[i], dice_items[min_index] = dice_items[min_index], dice_items[i]
+
+# dice_game_sorted: dict[str, int] = dict(dice_items)
+
+# print(f'\n{" RANKING DE JOGADORES ":=^26}')
+
+# ranking: int = 1
+# for player, dice in dice_game_sorted.items():
+#     print(f"{ranking}ª {player}: {dice}")
+#     ranking += 1
