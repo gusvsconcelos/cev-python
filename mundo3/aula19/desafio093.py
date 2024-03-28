817340f7942d609b4b35c4f180e9a69b93af889e
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-03-27 23:41:45 -0300

    Exercícios de Python (Aula 19)

---
+++ mundo3/aula19/desafio093.py
@@ -0,0 +1,25 @@
+print(f'{" DESAFIO 093 ":=^29}')
+
+player_data = {}
+scores = []
+player_data['Nome'] = input('Nome do jogador: ')
+
+matches = int(input(f'Quantas partidas {player_data["Nome"]} jogou? '))
+
+for i in range(matches):
+    scores.append(int(input(f'Quantos gols na partida {i + 1}? ')))
+
+player_data['Gols'] = scores
+player_data['Total'] = sum(scores)
+
+print('-=' * 25)
+
+for k, v in player_data.items():
+    print(f'{k}: {v}')
+
+print('-=' * 25)
+print(f'O jogador {player_data["Nome"]} jogou {matches} partidas.')
+
+for i in range(matches):
+    print(f' => Na partida {i + 1}, fez {player_data["Gols"][i]} gols.')
+
